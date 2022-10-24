6e3d35806e71cec201ad8293d6cf5b41b1bb8a72
Alvaro Cubiella <alvarocubiella@gmail.com>
2022-10-23 22:58:29 -0300

    Creo modelos para la db

--- blog_portal/admin.py
+++ blog_portal/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog_portal.models import Article, Portal, Publisher
+
+admin.site.register(Article)
+admin.site.register(Portal)
+admin.site.register(Publisher)
--- blog_portal/views.py
+++ blog_portal/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, TemplateView, View
+from blog_portal.models import Article, Portal
 
 def index(request):
     return render(request, 'blog_portal/index.html')
+
+class BaseView(View):
+
+     def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['headline'] = Article.objects.filter(is_headline=True).order_by('date_updated').first()
+        context['portal'] = Portal.objects.order_by('date_updated').first()
+        return context 
+
+class MainPageView(BaseView, ListView):
+    queryset = Article.objects.all()
+    context_object_name = "articles"
+    template_name = "blog_portal/index.html"
+
+class About(BaseView, TemplateView):
+
+    template_name = "blog_portal/about.html"
