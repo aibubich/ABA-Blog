a1043f55562ea0c95a3198d00e97ba69b9af49dc
aibubich <agusbubich@gmail.com>
2022-10-30 18:53:52 -0300

    busqueda de articulos por categoria usando id

--- blog_portal/views.py
+++ blog_portal/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, TemplateView, View
-from blog_portal.models import Article
+from django.views.generic import ListView, TemplateView, View, DetailView
+from blog_portal.models import Article, Category
 from panel_article import *
+
 
 def index(request):
     return render(request, 'blog_portal/index.html')
@@ -34,12 +35,26 @@ class About(BaseView, TemplateView):
     template_name = "blog_portal/about.html"
 
 
+
+#class ArticleDetailView(DetailView):
+
+    #model = Article
+    #context_object_name = "article"
+    
+    #def get_context_data(self, **kwargs):
+    #    context = super().get_context_data(**kwargs)
+    #    return contex
+
 class ArticleDetailView(TemplateView):
 
     template_name = "blog_portal/articulo.html"
-
-
-class SearchPostByCategory(ListView):
-    def get_queryset(self):
-        post_category = self.request.GET.get('post-category')
-        return Article.objects.filter(category=post_category)
+    
+def SearchPostByCategory(request):
+    qs = Article.objects.all()
+    post_category = request.GET.get('post-category')
+    if post_category != '' and post_category is not None:
+        qs = qs.filter(category=post_category)
+    context = {
+        'queryset':qs
+    }
+    return render (request, "blog_portal/article_filter.html", context) 
